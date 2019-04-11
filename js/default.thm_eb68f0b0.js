d0d35124960a9aeea826b9e96e2efc7564bb21e8
Phạm Quang Hưng <quanghung.pham@gmail.com>
2019-04-11 16:34:09 +0700

    first time upload
    upload new file

---
+++ js/default.thm_eb68f0b0.js
@@ -0,0 +1,108 @@
+window.skins={};
+                function __extends(d, b) {
+                    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+                        function __() {
+                            this.constructor = d;
+                        }
+                    __.prototype = b.prototype;
+                    d.prototype = new __();
+                };
+                window.generateEUI = {};
+                generateEUI.paths = {};
+                generateEUI.styles = undefined;
+                generateEUI.skins = undefined;generateEUI.paths['resource/fighter_skins/ButtonSkin.exml'] = window.skins.ButtonSkin = (function (_super) {
+	__extends(ButtonSkin, _super);
+	function ButtonSkin() {
+		_super.call(this);
+		this.skinParts = ["labelDisplay","iconDisplay"];
+		
+		this.minHeight = 50;
+		this.minWidth = 100;
+		this.elementsContent = [this._Image1_i(),this.labelDisplay_i(),this.iconDisplay_i()];
+		this.states = [
+			new eui.State ("up",
+				[
+				])
+			,
+			new eui.State ("down",
+				[
+					new eui.SetProperty("_Image1","source","button_down_png")
+				])
+			,
+			new eui.State ("disabled",
+				[
+					new eui.SetProperty("_Image1","alpha",0.5)
+				])
+		];
+	}
+	var _proto = ButtonSkin.prototype;
+
+	_proto._Image1_i = function () {
+		var t = new eui.Image();
+		this._Image1 = t;
+		t.percentHeight = 100;
+		t.scale9Grid = new egret.Rectangle(1,3,8,8);
+		t.source = "button_up_png";
+		t.percentWidth = 100;
+		return t;
+	};
+	_proto.labelDisplay_i = function () {
+		var t = new eui.Label();
+		this.labelDisplay = t;
+		t.bottom = 8;
+		t.fontFamily = "Tahoma 'Microsoft Yahei'";
+		t.left = 8;
+		t.right = 8;
+		t.size = 20;
+		t.textAlign = "center";
+		t.textColor = 0xFFFFFF;
+		t.top = 8;
+		t.verticalAlign = "middle";
+		return t;
+	};
+	_proto.iconDisplay_i = function () {
+		var t = new eui.Image();
+		this.iconDisplay = t;
+		t.horizontalCenter = 0;
+		t.verticalCenter = 0;
+		return t;
+	};
+	return ButtonSkin;
+})(eui.Skin);generateEUI.paths['resource/fighter_skins/startButtonSkin.exml'] = window.startButtonSkin = (function (_super) {
+	__extends(startButtonSkin, _super);
+	function startButtonSkin() {
+		_super.call(this);
+		this.skinParts = [];
+		
+		this.minHeight = 50;
+		this.minWidth = 100;
+		this.elementsContent = [];
+		this._Image1_i();
+		
+		this.states = [
+			new eui.State ("up",
+				[
+				])
+			,
+			new eui.State ("down",
+				[
+				])
+			,
+			new eui.State ("disabled",
+				[
+					new eui.AddItems("_Image1","",0,"")
+				])
+		];
+	}
+	var _proto = startButtonSkin.prototype;
+
+	_proto._Image1_i = function () {
+		var t = new eui.Image();
+		this._Image1 = t;
+		t.horizontalCenter = 0;
+		t.source = "btnStart";
+		t.verticalCenter = 1;
+		return t;
+	};
+	return startButtonSkin;
+})(eui.Skin);
